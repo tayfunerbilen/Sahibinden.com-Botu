6263d2b385a9dde7b7b483b6953be068ccf73ea8
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-02-14 15:07:47 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+header('Content-type: text/html; charset=utf8');
+require 'function.php';
+
+// ana kategoriler
+Sahibinden::Kategori();
+
+// alt kategoriler
+Sahibinden::Kategori('emlak');
+
+// kategori içerikleri
+Sahibinden::Liste('emlak');
+Sahibinden::Liste('emlak', 20); // 2. sayfa
+
+// içerik detayı (henüz tamamlanmadı)
+Sahibinden::Detay('http://www.sahibinden.com/ilan/emlak-konut-satilik-dorlion-gayrimenkul-den-yildiztepe-de-sifir-bina-da-2-plus1-153319984/detay');
