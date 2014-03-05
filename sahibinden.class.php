51ba615d52da9dec747b6f08b658b903ee1ac3b3
Tayfun Erbilen <tayfunerbilen@gmail.com>
2014-03-05 22:54:35 +0200

    Update sahibinden.class.php

--- sahibinden.class.php
+++ sahibinden.class.php
@@ -185,16 +185,14 @@ class Sahibinden
      */
     private function Curl( $url, $proxy = NULL )
     {
-        $options = array ( CURLOPT_RETURNTRANSFER => true, // return web page
-            CURLOPT_HEADER => false, // don't return headers
-            //CURLOPT_FOLLOWLOCATION => true, // follow redirects
-            CURLOPT_ENCODING => "", // handle compressed
-            CURLOPT_USERAGENT => "test", // who am i
-            CURLOPT_AUTOREFERER => true, // set referer on redirect
-            CURLOPT_CONNECTTIMEOUT => 30, // timeout on connect
-            CURLOPT_TIMEOUT => 30, // timeout on response
-            CURLOPT_MAXREDIRS => 10, // stop after 10 redirects
-            CURLOPT_SSL_VERIFYPEER => false // Disabled SSL Cert checks
+        $options = array ( CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false,
+            CURLOPT_ENCODING => "",
+            CURLOPT_AUTOREFERER => true,
+            CURLOPT_CONNECTTIMEOUT => 30,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_SSL_VERIFYPEER => false
         );
 
         $ch = curl_init( $url );
